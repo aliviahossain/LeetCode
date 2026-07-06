5b2ebd5e5e609aa396c5507e13cc55115a3adb67
Alivia Hossain <aliviahossainofficial@gmail.com>
2026-07-06 11:32:59 +0530

    Time: 0 ms (100%), Space: 44.7 MB (47.83%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums[0]>target)
+        return 0;
+        else if (nums[nums.length-1]<target)
+        return nums.length;
+    for (int i=1;i<nums.length;i++)
+    {
+        if ((nums[i]==target)||(nums[i-1]<target&&nums[i]>target))
+        return i;
+    } 
+    return 0;   
+    }
+}
